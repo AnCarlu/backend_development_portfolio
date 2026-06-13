010abd9e43c085e33890f96c0858db56c52d81ba
adria <adriancarrerolucha93@gmail.com>
2026-06-13 19:59:34 +0200

    map, filter and reduce functions

--- TemasAvanzados/README.md
+++ TemasAvanzados/README.md
@@ -5,3 +5,4 @@ Este modulo profundiza en temas avanzados de Python, como:
 * **Función zip en Python**
 * **Manejo de Cadenas** : función _split_, función _find_, función _replace_, la multiplicación de cadenas y la función _strip_
 * **Funciones Lambda** 
+* **Funciones _map()_, _filter()_ y _reduce()_ junto con lambda** 
--- TemasAvanzados/funciones_lambda.py
+++ TemasAvanzados/funciones_lambda.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 #Función cuadrado sin usar lambda
 def cuadrado(x):
     return x **2
@@ -9,3 +11,21 @@ print(f"El cuadrado de 5 es : {cuadrado(5)}")
 cuadrado_lambda = lambda x : x ** 2
 
 print(f'El cuadrado de 16 es: {cuadrado_lambda(16)}')
+
+#Con map y lambda se trabaja con valores iterables
+numeros = [1,2,3,4,5]
+
+#Aplicar la función lambda para obtener el cuadrado de cada numero
+cuadrados= list(map(lambda x: x ** 2, numeros))
+
+print(f'Resultado de usar map y lambda {cuadrados}')
+
+#Con filter y lambda
+pares= list(filter(lambda x : x % 2 == 0, numeros))
+
+print(f'Resultado de usar filter para buscar los pares : {pares}')
+
+# reduce y map
+suma_iterativa = reduce(lambda x, y: x + y, numeros)
+
+print(f'Suma acumulativa aplicando reduce: {suma_iterativa}')
