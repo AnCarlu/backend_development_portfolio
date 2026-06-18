3faae2c263b7c6a1757f46648ebd140b16f34536
adria <adriancarrerolucha93@gmail.com>
2026-06-18 17:40:31 +0200

    Lectura de archivos

---
+++ Manejo de archivos/lectura_archivos.py
@@ -0,0 +1,16 @@
+#Leer archivos en Python
+
+nombre_archivo = 'miarchivo.txt'
+
+#Leer archivos utilizando readlines
+with open(nombre_archivo, 'r') as archivo:
+    #print(archivo.readlines())
+    lineas=archivo.readlines()
+    for linea in lineas:
+        print(linea.strip())
+
+
+#Leer todo el contenido usando el método read()
+print('Leyendo el contenido con el método read')
+with open(nombre_archivo, 'r') as archivo:
+    print(archivo.read())
