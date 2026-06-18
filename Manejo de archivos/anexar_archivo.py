d14d88159d94896ec59c820eca58d7742bd93839
adria <adriancarrerolucha93@gmail.com>
2026-06-18 18:28:17 +0200

    Anezar información a archivos

---
+++ Manejo de archivos/anexar_archivo.py
@@ -0,0 +1,9 @@
+#Anexar información al archivo
+
+nombre_archivo='miarchivo.txt'
+
+with open(nombre_archivo, 'a') as archivo:
+    archivo.write('Anexando información....\n')
+    archivo.write('Saliendo de anexar información....\n')
+
+print(f'Se ha anexado información al archivo: {nombre_archivo}')
